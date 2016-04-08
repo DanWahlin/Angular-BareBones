3be90b9563e7fd17db1928532a5135301357813e
Dan Wahlin <dwahlin@xmlforasp.net>
2016-04-07 22:35:43 -0700

    Removed unnecessary imports

--- src/app/shared/app.providers.ts
+++ src/app/shared/app.providers.ts
@@ -1,6 +1,5 @@
-import { provide, bind } from 'angular2/core';
-import { NgLocalization } from 'angular2/common';
-import { HTTP_PROVIDERS, Http, XHRBackend, RequestOptions, ConnectionBackend } from 'angular2/http';
+import { bind } from 'angular2/core';
+import { HTTP_PROVIDERS } from 'angular2/http';
 import { FORM_PROVIDERS } from 'angular2/common';
 import { ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy } from 'angular2/router';
 
