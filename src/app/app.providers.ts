01d98cfb68b4fc6178e533a7fec438abd45085ae
Dan Wahlin <dwahlin@xmlforasp.net>
2016-04-26 23:05:21 -0700

    Update to beta 16

--- src/app/app.providers.ts
+++ src/app/app.providers.ts
@@ -1,7 +1,8 @@
 import { bind } from 'angular2/core';
 import { HTTP_PROVIDERS } from 'angular2/http';
 import { FORM_PROVIDERS } from 'angular2/common';
-import { ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy } from 'angular2/router';
+import { ROUTER_PROVIDERS } from 'angular2/router';
+import { LocationStrategy, HashLocationStrategy } from 'angular2/platform/common';
 
 import { DataService } from './shared/data.service';
 
