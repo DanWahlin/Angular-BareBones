a425c8171e7a880c240e58cd1bae1787bd41544e
Dan Wahlin <dwahlin@xmlforasp.net>
2016-06-23 23:39:37 -0700

    Removed unncessary import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ROUTER_DIRECTIVES, Router } from '@angular/router';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 import { APP_PROVIDERS } from './app.providers';
 
