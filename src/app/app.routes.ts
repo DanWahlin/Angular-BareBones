937dcdb701511e51f2c3eee1c6949f4c9594f4fc
Dan Wahlin <dwahlin@xmlforasp.net>
2016-06-26 11:39:38 -0700

    Eliminated unnecessary array in routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,10 @@ import { provideRouter, RouterConfig } from '@angular/router';
 import { HomeComponent }     from './home/home.component';
 import { FeatureComponent }   from './feature/feature.component';
 
-const appRoutes = [
+const routes: RouterConfig = [
   { path: '',  terminal: true, redirectTo: '/home' },
   { path: 'home',  terminal: true, component: HomeComponent },
   { path: 'feature', component: FeatureComponent }
-];
-
-const routes: RouterConfig = [
-  ...appRoutes
 ];
 
 export const APP_ROUTER_PROVIDERS = [
