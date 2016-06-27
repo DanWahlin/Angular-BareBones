eaa82db2ab25420e465e41716209c192e906180c
Dan Wahlin <dwahlin@xmlforasp.net>
2016-06-26 23:05:29 -0700

    Removed old FORM_PROVIDERS

--- src/app/app.providers.ts
+++ src/app/app.providers.ts
@@ -1,12 +1,11 @@
 import { bind } from '@angular/core';
 import { HTTP_PROVIDERS } from '@angular/http';
-import { FORM_PROVIDERS, LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { DataService } from './shared/data.service';
 
 export const APP_PROVIDERS = [
     DataService,
-    FORM_PROVIDERS,
     HTTP_PROVIDERS,
     //bind(LocationStrategy).toClass(HashLocationStrategy)
 ];
