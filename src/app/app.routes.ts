53d4c68050ea9e4cc6b0d463f46d52c7e1ae67d6
Dan Wahlin <dwahlin@xmlforasp.net>
2016-07-26 17:26:12 -0700

    Change const name for routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,12 @@ import { provideRouter, RouterConfig } from '@angular/router';
 import { HomeComponent }     from './home/home.component';
 import { FeatureComponent }   from './feature/feature.component';
 
-const routes: RouterConfig = [
+const APP_ROUTES: RouterConfig = [
   { path: '',  pathMatch:'full', redirectTo: '/home' },
   { path: 'home',  component: HomeComponent },
   { path: 'feature', component: FeatureComponent }
 ];
 
 export const APP_ROUTER_PROVIDERS = [
-  provideRouter(routes)
+  provideRouter(APP_ROUTES)
 ];
