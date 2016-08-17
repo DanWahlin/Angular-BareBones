1c42a8830df7bdf13ab0b816be1325a404581d17
Dan Wahlin <dwahlin@xmlforasp.net>
2016-08-17 16:35:46 -0700

    Naming changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,11 @@ import { HttpModule} from '@angular/http';
 import { AppComponent }  from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FeatureComponent } from './feature/feature.component';
-import { APP_ROUTING } from './app.routing';
+import { app_routing } from './app.routing';
 import { DataService } from './shared/data.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpModule, APP_ROUTING ],
+  imports:      [ BrowserModule, FormsModule, HttpModule, app_routing ],
   declarations: [ AppComponent, HomeComponent, FeatureComponent ],
   providers:    [ DataService ],
   bootstrap:    [ AppComponent ]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,10 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent }     from './home/home.component';
 import { FeatureComponent }   from './feature/feature.component';
 
-const APP_ROUTES: Routes = [
+const app_routes: Routes = [
   { path: '',  pathMatch:'full', redirectTo: '/home' },
   { path: 'home',  component: HomeComponent },
   { path: 'feature', component: FeatureComponent }
 ];
 
-export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
+export const app_routing = RouterModule.forRoot(app_routes);
