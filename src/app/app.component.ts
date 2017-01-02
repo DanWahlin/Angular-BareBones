91051d052f1edf1aa85b6dc058cf865176fb34e8
Dan Wahlin <danwahlin@Dans-iMac.local>
2017-01-02 14:16:48 -0700

    Rename selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({ 
-  selector: 'app-container',
+  selector: 'app-component',
   template: `<router-outlet></router-outlet>`
 })
 export class AppComponent {
