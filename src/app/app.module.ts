1db762ec74cf7bbc3ecc56ccb481649d1feaff68
Dan Wahlin <danwahlin@Dans-MacBook-Pro.local>
2017-07-25 16:54:51 -0700

    Fixed thanks to Chad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule} from '@angular/http';
 
 import { AppComponent }  from './app.component';
-import { HomeComponent } from './home/home.component';
-import { FeatureComponent } from './feature/feature.component';
 import { appRouting } from './app.routing';
 import { DataService } from './shared/data.service';
 
